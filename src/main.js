7ccc8858df09684c2ad04076ffbb226e2b2359fa
maxschmi <maxschm@yahoo.de>
2024-05-08 16:52:20 +0200

    create single file build

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 import 'bootstrap'
 
+import favicon from './assets/favicon.ico'
 import App from './App.vue'
 
+// Create the favicon link element
+const link = document.createElement('link')
+link.rel = 'icon'
+link.href = favicon
+document.head.appendChild(link)
+
+// create the app instance
 const app = createApp(App)
 
 const pinia = createPinia()
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    viteSingleFile({ removeViteModuleLoader: true })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    assetsInlineLimit: 24 * 1024,
   }
 })
